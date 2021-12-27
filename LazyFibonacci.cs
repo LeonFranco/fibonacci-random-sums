390b51ff7b55e717ec8268e586031433b99d9ff8
Leonard Franco <leonard.k.franco@gmail.com>
2021-12-27 12:02:03 -0800

    Change LazyFibonacci class to use auto property

--- LazyFibonacci.cs
+++ LazyFibonacci.cs
@@ -2,32 +2,32 @@ using System.Numerics;
 
 public class LazyFibonacci
 {
-    private List<BigInteger> sequence;
+    protected List<BigInteger> Sequence { get; private set; }
 
     public LazyFibonacci()
     {
-        this.sequence = new List<BigInteger>();
-        this.sequence.Add(0);
-        this.sequence.Add(1);
+        this.Sequence = new List<BigInteger>();
+        this.Sequence.Add(0);
+        this.Sequence.Add(1);
     }
 
     public BigInteger Get(int seqIndex)
     {
-        if (this.sequence.Count <= seqIndex)
+        if (this.Sequence.Count <= seqIndex)
         {
             this.UpdateSequence(seqIndex);
         }
 
-        return sequence[seqIndex];
+        return Sequence[seqIndex];
     }
 
     private void UpdateSequence(int seqIndex)
     {
-        for (int i = this.sequence.Count; i <= seqIndex; ++i) {
-            BigInteger firstTerm = this.sequence[i - 2];
-            BigInteger secondTerm = this.sequence[i - 1];
+        for (int i = this.Sequence.Count; i <= seqIndex; ++i) {
+            BigInteger firstTerm = this.Sequence[i - 2];
+            BigInteger secondTerm = this.Sequence[i - 1];
             BigInteger nextFibNum = firstTerm + secondTerm;
-            this.sequence.Add(nextFibNum);
+            this.Sequence.Add(nextFibNum);
         }
     }
 }
